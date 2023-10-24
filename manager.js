7d5a49b8a2c886676ca9f8af638e2738f0c068ef
Collin <collinullmann@gmail.com>
2023-10-24 19:31:09 -0400

    finished through phase 04

--- manager.js
+++ manager.js
@@ -2,18 +2,46 @@ const Employee = require('./employee');
 
 
 class Manager extends Employee {
-    constructor(name, salary, title, manager, employees = []){
+    constructor(name, salary, title, manager, employees = []) {
         super(name, salary, title, manager)
 
         this.employees = employees;
+
     }
 
-    addEmployee(employee){
+    addEmployee(employee) {
         this.employees.push(employee);
     }
+
+    calculateBonus(multiplier) {
+        return (this.salary + this._totalSubSalary()) * multiplier
+    }
+
+    _totalSubSalary() {
+        let sum = 0;
+        for (let employee of this.employees) {
+            if (employee instanceof Manager) {
+                sum += employee.salary + employee._totalSubSalary()
+            } else {
+                sum += employee.salary
+            }
+        }
+        return sum
+    }
+
+
 }
 
-// const splinter = new Manager('Splinter', 100000, 'Sensei');
-// console.log('Before: ', splinter);
+
+const splinter = new Manager('Splinter', 100000, 'Sensei');
+const leo = new Manager('Leonardo', 90000, 'Ninja', splinter);
+const raph = new Manager('Raphael', 90000, 'Ninja', leo);
+const mikey = new Employee('Michelangelo', 85000, 'Grasshopper', raph);
+const donnie = new Employee('Donatello', 85000, 'Grasshopper', raph);
+
+console.log(splinter.calculateBonus(0.05)); // => 22500
+// console.log(leo.calculateBonus(0.05)); // => 17500
+// console.log(raph.calculateBonus(0.05)); // => 13000
+
 
 module.exports = Manager;
